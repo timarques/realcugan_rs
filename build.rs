9daa40301e50fade887b5dbf44029973d8d00d4b
Tiago Marques <tyimarques@gmail.com>
2024-09-25 08:19:55 +0100

    fix shaders

--- build.rs
+++ build.rs
@@ -1,6 +1,9 @@
 fn main() {
     println!("cargo::rerun-if-changed=src");
-    let dst = cmake::Config::new("src").build();
+    let dst = cmake::Config::new("src")
+        .cflag("-w")
+        .build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
     println!("cargo:rustc-link-lib=static:-bundle={}", "realcugan-wrapper");
+    println!("cargo:rustc-link-lib=dylib={}", "stdc++");
 }
