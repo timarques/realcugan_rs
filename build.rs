916295531cf6ae0398a514c10869981677b71c1f
Tiago Marques <tyimarques@gmail.com>
2024-09-29 13:25:03 +0100

    improve build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,10 @@
 fn main() {
     let dst = cmake::Config::new("src").build();
     println!("cargo:rustc-link-search=native={}/lib", dst.display());
-    println!("cargo:rustc-link-lib=static:-bundle={}", "realcugan-wrapper");
-    println!("cargo:rustc-link-lib=dylib={}", "stdc++");
+    println!("cargo:rustc-link-lib=static={}", "realcugan-wrapper");
     println!("cargo:rustc-link-lib=dylib={}", "ncnn");
+    if cfg!(unix) {
+        println!("cargo:rustc-link-lib=dylib={}", "stdc++");
+        println!("cargo:rustc-link-lib=dylib={}", "pthread");
+    }
 }
